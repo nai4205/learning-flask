c9dcb94a71f8c1e3ac402ec21117a82d7e4304a2
nai4205 <naijonkorn@gmail.com>
2023-08-30 23:20:55 +0100

    Loop through blog posts and update title if one is passed in

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'Nai korn',
+        'title' : 'Blog post 1',
+        'content' : 'First post',
+        'date_posted' : 'July 2, 2017'
+    },
+    {
+        'author' : 'Aneurin korn',
+        'title' : 'Blog post 2',
+        'content' : 'Second post',
+        'date_posted' : 'July 4, 2017'
+    }    
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home page!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About!</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
