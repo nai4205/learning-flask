135594fa9a247460537e62eb7457052c2ea00b5e
nai4205 <naijonkorn@gmail.com>
2023-08-30 22:57:07 +0100

    Basics of routing to different pages

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, world!"
+@app.route("/home")
+def home():
+    return "<h1>Home page!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
