b77644a4150949214e0ab8f0b396e9593086f10a
nai4205 <naijonkorn@gmail.com>
2023-09-15 00:49:05 +0100

    Register and login

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '622bf6829e152f0f86a46ad7f682852a'
 
 posts = [
     {
@@ -24,6 +26,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
