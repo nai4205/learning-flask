717ebb4a8bc992ce09a32b4d6f3ab63fcf77587d
nai4205 <naijonkorn@gmail.com>
2023-09-17 18:35:44 +0100

    lgtm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '622bf6829e152f0f86a46ad7f682852a'
@@ -27,9 +27,13 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
+        
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
